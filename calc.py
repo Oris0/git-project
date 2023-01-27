eafd76d6cdeb083ab04e73a8144e1b0cf4901a85
larry3601 <odenigbolarry@gmail.com>
2023-01-27 01:28:55 -0800

    calc update new

--- calc.py
+++ calc.py
@@ -1,6 +1,18 @@
+ans = input("what will you want to do S or A: ")
+
+
+
+
 x = float( input("insert first num : "))
-y =  float(input("insert second num : "))
+y =  float(input("insert second num: "))
 
-results = x * y
+if ans == "S":
+   result = x-y
+   print (f"{result}")
+elif ans== "a":
+   result = x * y
+   print (f"{result}")
 
-print (f"{results}")
+else: 
+    print ("useyourhead")
+
